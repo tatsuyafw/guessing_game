2f727eada67749020e97677d680b96bd5d7b94ea
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2022-04-04 22:44:33 +0900

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please inputy your guess.");
+        println!("Please input your guess.");
 
         let mut guess = String::new();
 
